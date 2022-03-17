60350e7e860363f79a94ce045e62b50f421de342
NabiylaHamestuti14 <naufalhisyam266@gmail.com>
2022-03-17 21:36:18 +0700

    Update Icon.png
    menggabungkan komponen

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,17 +2,24 @@ import  "bootstrap/dist/css/bootstrap.css";
 //import React from "react";
 import "./Login.css";
 import React, {Component} from 'react';
-import Uname from './componen/uname';
-import Gambar from "./componen/Gambar";
+import Logowelcome from './Icon.png'
 
 class Login  extends Component {
     render() {
         return (
             <div className="main-body">
+                <div>
+            <h2 className="title">Choose Youre 
+            <br/>The Best Skincare</h2>
+            
+            <img className="image" src={Logowelcome} alt="logo"/>
+            </div>
                 <div className="body">
-                    <Uname />
-                    <Passw />
-                    <Gambar/>
+                <div className='font-poppins'>
+                <h3>Hi, Welcome Back!</h3>
+                <input type="text" className="username" placeholder="Username"/>
+                <input type="text" className="password" placeholder="Password"/>
+            </div>
                 </div>
             </div> 
         );

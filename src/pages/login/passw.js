9552e4ba279ac7af51fca76be6ce5d1408c7149e
rfkrta <rafikartaw2@gmail.com>
2022-03-18 11:39:50 +0700

    Update passw.js

--- src/pages/login/passw.js
+++ src/pages/login/passw.js
@@ -4,13 +4,8 @@ class Passw extends Component {
     render(){
         return (
             <div>
-<<<<<<< HEAD
             <label htmlFor="Password" className="form-label" class = 'mt-5'>Password</label>
             <input className='form-control' type="text" id='password'/>
-=======
-            <label htmlFor="Password" className="form-label">Password</label>
-            <input className='form-control' type="password" id='password' securityTextEntry={true}/>
->>>>>>> 7bcc17e43ac8e51194052c455d59c5362ebb1b40
             </div>
         )
     }

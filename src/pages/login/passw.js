4aef83f50895528679f01a10a44e736b04c9de22
Khoirunnisa <k.nisa11602@gmail.com>
2022-03-18 11:24:45 +0700

    edit css

--- src/pages/login/passw.js
+++ src/pages/login/passw.js
@@ -4,7 +4,7 @@ class Passw extends Component {
     render(){
         return (
             <div>
-            <label htmlFor="Password" className="form-label">Password</label>
+            <label htmlFor="Password" className="form-label mt-4">Password</label>
             <input className='form-control' type="text" id='password'/>
             </div>
         )

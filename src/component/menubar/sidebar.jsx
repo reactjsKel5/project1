a697b43e98c7d4619b6b4f7cbd88fc38a09f2a2c
pujirahayu01 <74763414+pujirahayu01@users.noreply.github.com>
2022-04-07 12:58:57 +0700

    update sidebar router

--- src/component/menubar/sidebar.jsx
+++ src/component/menubar/sidebar.jsx
@@ -1,5 +1,83 @@
+// import React, { Component } from "react";
+// import './menubar.css';
+
+// class Sidebar extends Component {
+//     render() {
+//         return (
+//             <div className="container1">
+//                 <div className="navigation">
+//                     <ul>
+//                         <li>
+//                             <a href="#">
+//                                 <span className="icon">
+//                                     <ion-icon name="school-outline"></ion-icon>
+//                                 </span>
+//                                 <span className="title">Brand Name</span>
+//                             </a>
+//                         </li>
+//                         <li>
+//                             <a href="#">
+//                                 <span className="icon">
+//                                     <ion-icon name="home-outline"></ion-icon>
+//                                 </span>
+//                                 <span className="title">Dashboard</span>
+//                             </a>
+//                         </li>
+//                         <li>
+//                             <a href="#">
+//                                 <span className="icon">
+//                                     <ion-icon name="today-outline"></ion-icon>
+//                                 </span>
+//                                 <span className="title">College Management</span>
+//                             </a>
+//                         </li>
+//                         <li>
+//                             <a href="#">
+//                                 <span className="icon">
+//                                     <ion-icon name="cash-outline"></ion-icon>
+//                                 </span>
+//                                 <span className="title">Money Management</span>
+//                             </a>
+//                         </li>
+//                         <li>
+//                             <a href="#">
+//                                 <span className="icon">
+//                                     <ion-icon name="person-circle-outline"></ion-icon>
+//                                 </span>
+//                                 <span className="title">Edit Profile</span>
+//                             </a>
+//                         </li>
+//                         <li className="mt-5">
+//                             <a href="#">
+//                                 <span className="icon">
+//                                     <ion-icon name="log-out-outline"></ion-icon>
+//                                 </span>
+//                                 <span className="title">Log-out</span>
+//                             </a>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Sidebar;
+
+
 import React, { Component } from "react";
 import './menubar.css';
+import Dashboard from "../../container/dashboard/dashboard";
+import MenuCM from "../../container/menuCM/menuCM";
+import MenuMM from "../../container/menuMM/menuMM";
+
+import {
+    Link
+
+} from "react-router-dom";
+
+
+
 
 class Sidebar extends Component {
     render() {
@@ -16,50 +94,51 @@ class Sidebar extends Component {
                             </a>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/">
                                 <span className="icon">
                                     <ion-icon name="home-outline"></ion-icon>
                                 </span>
                                 <span className="title">Dashboard</span>
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/CollegeManagement">
                                 <span className="icon">
                                     <ion-icon name="today-outline"></ion-icon>
                                 </span>
                                 <span className="title">College Management</span>
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/MoneyManagement">
                                 <span className="icon">
                                     <ion-icon name="cash-outline"></ion-icon>
                                 </span>
                                 <span className="title">Money Management</span>
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a href="#">
+                            <Link to="/editProfile">
                                 <span className="icon">
                                     <ion-icon name="person-circle-outline"></ion-icon>
                                 </span>
                                 <span className="title">Edit Profile</span>
-                            </a>
+                            </Link>
                         </li>
-                        <li className="mt-5">
+                        {/* <li className="mt-5">
                             <a href="#">
                                 <span className="icon">
                                     <ion-icon name="log-out-outline"></ion-icon>
                                 </span>
                                 <span className="title">Log-out</span>
                             </a>
-                        </li>
+                        </li> */}
                     </ul>
                 </div>
-            </div>
+            </div >
         );
     }
 }
+
 
 export default Sidebar;

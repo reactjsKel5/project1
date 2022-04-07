2d55eeec0d5d3798a9be12caa03c09b8c6d31af0
pujirahayu01 <74763414+pujirahayu01@users.noreply.github.com>
2022-04-07 12:59:58 +0700

    update app router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './container/dashboard/dashboard';
-// import Login from './component/login';
+import Dashboard from "../src/container/dashboard/dashboard";
+import MenuCM from "../src/container/menuCM/menuCM";
+import MenuMM from "../src/container/menuMM/menuMM";
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Dashboard/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/"><Dashboard /></Route>
+        <Route exact path="/CollegeManagement"><MenuCM /></Route>
+        <Route exact path="/MoneyManagement"><MenuMM /></Route>
+      </Switch>
+    </Router>
   );
 }
 
